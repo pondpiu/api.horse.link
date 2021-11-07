72e5742310ecf572c142a18e9a4dbadc612830f9
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-11-07 11:07:36 +1000

    Feat: Add routes & cache

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # api.horse.link
 NodeJS API for horse link
 
+# Introduction
+This project allows users to create a signed data to add to Horse Link dapps.
+
 ## Starting
 
 ```bash
     npm install
     npm start
 ```
+
+## Requests
--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
 const express = require("express");
+const cache = require("memory-cache");
+const axios = require("axios");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 
-app.get("/", (req, res) => {
-  res.json({ user: "giddy up" });
+const getName = (item) => {
+  return [item.firstname, item.lastname].join(" ");
+};
+
+//
+app.get("/meetings", async (req, res) => {
+  const config = {
+    method: "get",
+    url: "https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/2021-11-07/meetings?jurisdiction=QLD&returnOffers=true&returnPromo=false",
+    headers: {},
+  };
+
+  const response = await axios(config);
+  const meetings = response.data.meetings.map((item) => item.meetingName);
+  cache.put("meetings", meetings, 1000 * 60 * 60);
+  console.log(meetings);
+
+  res.json(cache.get("meetings"));
 });
 
-app.listen(PORT, (err)  => {
+//
+app.get("/meetings/:date", async (req, res) => {
+  const config = {
+    method: "get",
+    url: `https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/${req.params.date}/meetings?jurisdiction=QLD&returnOffers=true&returnPromo=false`,
+    headers: {},
+  };
+
+  const response = await axios(config);
+  const meetings = response.data.meetings.map((item) => item.meetingName);
+  cache.put("meetings", meetings, 1000 * 60 * 60);
+  console.log(meetings);
+
+  res.json(cache.get("meetings"));
+});
+
+app.listen(PORT, (err) => {
   if (err) console.log(err);
   console.log("Server listening on PORT", PORT);
 });
